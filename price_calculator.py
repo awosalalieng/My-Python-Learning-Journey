7563eab71f18ed7a10bce1c0af3f906bbeafa194
AWOS <awosalali.eng@gmail.com>
2026-08-30 09:09:42 +0800

    Create price_calculator.py

---
+++ price_calculator.py
@@ -0,0 +1,8 @@
+price = input("How much is product? :\n")
+   float_price = float(price)
+   
+   amount = input("Enter the amount of products?:\n")
+   int_amount = int(amount)
+   
+   result = float_price * int_amount
+   print("The total is: " + str(result))
